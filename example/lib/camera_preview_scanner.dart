cc74369266b03be77ef3e29605b4696646991e85
Kyaw <mbikyaw@nus.edu.sg>
2021-07-02 12:36:55 +0800

    firebase cloud messaging

--- example/lib/camera_preview_scanner.dart
+++ example/lib/camera_preview_scanner.dart
@@ -86,6 +86,8 @@ class _CameraPreviewScannerState extends State<CameraPreviewScanner> {
         return _imageLabeler.processImage;
       case Detector.face:
         return _faceDetector.processImage;
+      case Detector.cloudLabel:
+        break;
     }
 
     return null;
